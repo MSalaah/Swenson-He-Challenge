713e56449ed669b5b5a769ef3972aa3638392bba
Mohamed <msalah.dev@gmail.com>
2021-04-23 13:00:09 +0200

    Initial Commit

---
+++ ioschallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ioschallenge
+//
+//  Created by Mohamed Salah on 4/23/21.
+//  Copyright © 2021 SWENSON HE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ioschallengeTests/ioschallengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  ioschallengeTests.swift
+//  ioschallengeTests
+//
+//  Created by Mohamed Salah on 4/23/21.
+//  Copyright © 2021 SWENSON HE. All rights reserved.
+//
+
+import XCTest
+@testable import ioschallenge
+
+class ioschallengeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
